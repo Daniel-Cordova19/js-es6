0636b10a416f27a19ef1c1064e37fce9cd313bc0
Daniel Cordova <javierdanielcordova@gmail.com>
2023-05-25 10:34:59 +0200

    ejercicio 3 listo sin el D

--- ex3.js
+++ ex3.js
@@ -6,13 +6,13 @@
 
 let numeros = [1, 2, 3];
 
-let nuevoArray = numeros.map(/** TODO */);
-console.log("Apartado A: ", nuevoArray); // [0 , 1, 2];
+let nuevoArray = numeros.map(numero => numero -1);
+//console.log("Apartado A: ", nuevoArray); // [0 , 1, 2];  //Resuelto🆗
 
 // B) Usa la función map para transformar a mayúsculas todos los strings de este array
 let animales = ["vertín, bigotes"];
-let nuevosAnimales; // TODO
-// console.log("Apartado B: ", nuevosAnimales) // ["VERTÍN, BIGOTES"]
+let nuevosAnimales = animales.map(animal => animal.toUpperCase())
+//console.log("Apartado B: ", nuevosAnimales) // ["VERTÍN, BIGOTES"] //Resuelto🆗
 
 
 // C) Usa la función map para actualizar con la fecha de hoy (new Date().toLocaleString()) la propiedad ultimaVisita de todos los pacientes de una clínica dental
@@ -26,15 +26,19 @@ let pacientes = [{
     ultimaVisitaPagada: true,
     ultimaVisita: '4/4/2022 18: 32: 19'
 }];
-let pacientesVisitados; // TODO
-// console.log("Apartado C: ", pacientesVisitados) // La propiedad 'ultimaVisita' de TODOS los pacientes debe estar actualizada a fecha de hoy
+let pacientesVisitados= pacientes.map(paciente=> paciente.ultimaVisita = (new Date().toLocaleString()))
+//console.log("Apartado C: ", pacientesVisitados) // La propiedad 'ultimaVisita' de TODOS los pacientes debe estar actualizada a fecha de hoy //Resuelto🆗
 
-// D) DIFÍCIL. Usa la función map para AGREGAR un nuevo campo al array de pacientes. El campo debe llamarse 'avisos'. Si el paciente NO ha pagado la última visita, dicho campo debe contener el string 'Paciente moroso, cobrar!'; en caso contrario, debe dejarse vacío. BONUS: usa un condicional ternario. BONUS: Usa el operador ...object para no tener que escribir todos los campos.
 
-let pacientesAvisos; // TODO
-// console.log("Apartado D: ", pacientesAvisos);
-/**
- * {
+
+
+// D) DIFÍCIL. Usa la función map para AGREGAR un nuevo campo al array de pacientes. 
+//El campo debe llamarse 'avisos'. 
+//Si el paciente NO ha pagado la última visita, dicho campo debe contener el string 'Paciente moroso, cobrar!'; 
+//en caso contrario, debe dejarse vacío. 
+//BONUS: usa un condicional ternario. BONUS: Usa el operador ...object para no tener que escribir todos los campos.
+
+/*let cositas = [{
     nombre: "Pedro",
     ultimaVisitaPagada: false,
     ultimaVisita: '4/4/2022 18: 32: 19',
@@ -44,12 +48,13 @@ let pacientesAvisos; // TODO
     ultimaVisitaPagada: true,
     ultimaVisita: '4/4/2022 18: 32: 19',
     avisos: ''
-}
- */
+}]
 
+let pacientesAvisos = cositas.map(paciente => paciente.avisos = 'Paciente moroso, cobrar!'? (paciente.nombre + " Paciente moroso, cobrar!") :'' );
+console.log("Apartado D: ", pacientesAvisos);
 
 // E) Usa la función map para crear tantos tags <li> como pacientes hay en el array
-let pacientesLista; // TODO
+//let pacientesLista; // TODO
 // console.log("Apartado E: ", pacientesLista);
 
 /**
